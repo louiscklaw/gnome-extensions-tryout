080dc5832e6d4b8d0645dbb8179fe58919ed0c45
Louis Law <louiscklaw@users.noreply.github.com>
2023-09-09 14:39:15 +0800

    update,

---
+++ gjs-helloworld/function-helloworld/index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/gjs
+
+imports.gi.versions.Gtk = '3.0';
+const Gtk = imports.gi.Gtk;
+
+imports.searchPath.push('.');
+
+const sampleFunc = imports.sampleFunc;
+const util1 = imports.utils.util1;
+
+// let win = new Gtk.Window();
+// win.connect("delete-event", Gtk.main_quit);
+// win.show_all();
+// Gtk.main();
+
+function main() {
+  log('main start');
+  sampleFunc.helloworld();
+  util1.helloworld();
+}
+
+main();
---
+++ gjs-helloworld/helloworld/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/gjs
+
+imports.gi.versions.Gtk = '3.0';
+const Gtk = imports.gi.Gtk;
+// Gtk.init(null);
+
+// let win = new Gtk.Window();
+// win.connect("delete-event", Gtk.main_quit);
+// win.show_all();
+// Gtk.main();
+
+log('helloworld')
