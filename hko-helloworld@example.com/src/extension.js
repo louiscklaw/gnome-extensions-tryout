c6cf44fefc71d9b7f5578345914127560d5ff286
Louis Law <louiscklaw@users.noreply.github.com>
2023-09-11 10:17:31 +0800

    update hko-weather@example.com,

--- hko-helloworld@example.com/src/extension.js
+++ hko-helloworld@example.com/src/extension.js
@@ -41,3 +41,38 @@ function disable() {
   Mainloop.source_remove(timeout);
   Main.panel._rightBox.remove_child(panelButton);
 }
+
+
+function getHkoJsonGet(url) {
+  var output = { state: 'start', debug: {}, error: '' };
+  try {
+    let sessionSync = new Soup.SessionSync();
+
+    // https://www.hko.gov.hk/en/abouthko/opendata_intro.htm
+    let msg = Soup.Message.new('GET', url);
+    sessionSync.send_message(msg);
+    res_json = JSON.parse(msg.response_body.data);
+
+    output = { ...output, state: 'done', result: res_json };
+  } catch (error) {
+    output = { ...output, state: 'error', error: error.message };
+    console.error(error.message);
+  }
+
+  return output;
+}
+
+function localWeatherForecastGet() {
+  var test_url_en =
+    'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=en';
+  var test_url_tc =
+    'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=tc';
+
+  var output = { state: 'start', debug: {}, error: '' };
+  output = getHkoJsonGet(test_url_en);
+  output = {...output, debug:{
+    test_url_en, test_url_tc,
+  }}
+
+  return output;
+}
