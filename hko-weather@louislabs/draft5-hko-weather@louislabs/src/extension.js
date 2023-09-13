49a3344f59167b65a42b499a2fdeff4312da9795
Louis Law <louiscklaw@users.noreply.github.com>
2023-09-14 02:19:11 +0800

    update proto1-hko-weather@louislabs,

--- hko-weather@louislabs/draft5-hko-weather@louislabs/src/extension.js
+++ hko-weather@louislabs/draft5-hko-weather@louislabs/src/extension.js
@@ -96,72 +96,63 @@ const MyPopup = GObject.registerClass(
         x_align: Clutter.ActorAlign.CENTER,
       });
 
+      // current_weather -> temperature
+      let current_weather_box = new St.BoxLayout({
+        x_expand: true,
+      });
 
-        // current_weather -> temperature
-        let current_weather_box = new St.BoxLayout({
-          x_expand: true,
-        });
+      let current_weather_temperature = new St.BoxLayout({
+        vertical: true,
+        x_expand: true,
+        x_align: Clutter.ActorAlign.START,
+      });
 
-          let current_weather_temperature = new St.BoxLayout({
-            vertical: true,
-            x_expand: true,
-            x_align: Clutter.ActorAlign.START,
-          });
-
-            current_weather_temperature.add_actor(
-              new St.Label({
-                text: 'Temperature',
-                style: 'font-weight: bold; padding-top: 10px; padding-bottom: 10px;',
-              }),
-            );
-            current_weather_temperature.add_actor(
-              new St.Label({
-                text: '26°C',
-                style: 'font-size: 40px; padding-top: 10px; padding-bottom: 10px; color: #ecf0f1',
-              }),
-            );
-          
-        current_weather_box.add_actor(current_weather_temperature);
-        // current_weather -> temperature
-
-        // current_weather -> humidity
-        let current_weather_humidity = new St.BoxLayout({
-          vertical: true,
-          x_expand: true,
-          x_align: Clutter.ActorAlign.END,
-        });
-
-          current_weather_humidity.add_actor(
-            new St.Label({
-              text: 'Humidity:',
-              style: 'font-weight: bold; padding-top: 10px; padding-bottom: 10px;',
-            }),
-          );
-
-          current_weather_humidity.add_actor(
-            new St.Label({
-              text: '98%',
-              style: 'font-size: 40px; padding-top: 10px; padding-bottom: 10px; color: #ecf0f1',
-            }),
-          );
-        // current_weather -> humidity
-      
-        
-
-      current_weather_widget.add_actor(
-        current_weather_box,
+      current_weather_temperature.add_actor(
+        new St.Label({
+          text: 'Temperature',
+          style: 'font-weight: bold; padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
+      current_weather_temperature.add_actor(
+        new St.Label({
+          text: '26°C',
+          style:
+            'font-size: 40px; padding-top: 10px; padding-bottom: 10px; color: #ecf0f1',
+        }),
       );
 
-      current_weather_box.add_actor(
-        current_weather_humidity
+      current_weather_box.add_actor(current_weather_temperature);
+      // current_weather -> temperature
+
+      // current_weather -> humidity
+      let current_weather_humidity = new St.BoxLayout({
+        vertical: true,
+        x_expand: true,
+        x_align: Clutter.ActorAlign.END,
+      });
+
+      current_weather_humidity.add_actor(
+        new St.Label({
+          text: 'Humidity:',
+          style: 'font-weight: bold; padding-top: 10px; padding-bottom: 10px;',
+        }),
       );
+
+      current_weather_humidity.add_actor(
+        new St.Label({
+          text: '98%',
+          style:
+            'font-size: 40px; padding-top: 10px; padding-bottom: 10px; color: #ecf0f1',
+        }),
+      );
+      // current_weather -> humidity
+
+      current_weather_widget.add_actor(current_weather_box);
+
+      current_weather_box.add_actor(current_weather_humidity);
 
       this.menu.box.add_child(current_weather_widget);
       // current_weather_widget
-
-
-
-
 
       // forecast_widget
       let forecast_widget = new St.Widget({
@@ -171,40 +162,40 @@ const MyPopup = GObject.registerClass(
 
       // weather_forecast -> day 1
       let forecast_boxes = new St.BoxLayout({
-        style: 'background-color: gold;',
+        // style: 'background-color: gold;',
         x_expand: true,
       });
-        let weather_forecast_day1 = new St.BoxLayout({
-          vertical: true,
-          x_expand: true,
-          x_align: Clutter.ActorAlign.START,
-        });
+      let weather_forecast_day1 = new St.BoxLayout({
+        vertical: true,
+        x_expand: true,
+        x_align: Clutter.ActorAlign.START,
+      });
 
-          weather_forecast_day1.add_actor(
-            new St.Label({
-              text: '1/Sep',
-              style: 'padding-top: 10px; padding-bottom: 10px;',
-            }),
-          );
-          weather_forecast_day1.add_actor(
-            new St.Icon({
-              icon_size: 32,
-              gicon: Gio.icon_new_for_string(
-                Me.dir.get_path() + '/svgs/weather/clear-day.svg',
-              ),
-              y_expand: true,
-              y_align: Clutter.ActorAlign.CENTER,
-              x_expand: true,
-              x_align: Clutter.ActorAlign.CENTER,
-              style: 'padding-top: 10px; padding-bottom: 10px;',
-            }),
-          );
-          weather_forecast_day1.add_actor(
-            new St.Label({
-              text: '26°C',
-              style: 'padding-top: 10px; padding-bottom: 10px;',
-            }),
-          );
+      weather_forecast_day1.add_actor(
+        new St.Label({
+          text: '1/Sep',
+          style: 'padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
+      weather_forecast_day1.add_actor(
+        new St.Icon({
+          icon_size: 32,
+          gicon: Gio.icon_new_for_string(
+            Me.dir.get_path() + '/svgs/weather/clear-day.svg',
+          ),
+          y_expand: true,
+          y_align: Clutter.ActorAlign.CENTER,
+          x_expand: true,
+          x_align: Clutter.ActorAlign.CENTER,
+          style: 'padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
+      weather_forecast_day1.add_actor(
+        new St.Label({
+          text: '26°C',
+          style: 'padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
       forecast_boxes.add_actor(weather_forecast_day1);
       // weather_forecast -> day 1
 
@@ -215,33 +206,33 @@ const MyPopup = GObject.registerClass(
         x_align: Clutter.ActorAlign.CENTER,
       });
 
-        weather_forecast_day2.add_actor(
-          new St.Label({
-            text: '1/Sep',
-            style: 'padding-top: 10px; padding-bottom: 10px;',
-          }),
-        );
-        weather_forecast_day2.add_actor(
-          new St.Icon({
-            icon_size: 32,
-            gicon: Gio.icon_new_for_string(
-              Me.dir.get_path() + '/svgs/weather/clear-day.svg',
-            ),
-            y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER,
-            x_expand: true,
-            x_align: Clutter.ActorAlign.CENTER,
-            style: 'padding-top: 10px; padding-bottom: 10px;',
-          }),
-        );
-        weather_forecast_day2.add_actor(
-          new St.Label({
-            text: '26°C',
-            style: 'padding-top: 10px; padding-bottom: 10px;',
-          }),
-        );
-        forecast_boxes.add_actor(weather_forecast_day2);
-    // weather_forecast -> day 2
+      weather_forecast_day2.add_actor(
+        new St.Label({
+          text: '1/Sep',
+          style: 'padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
+      weather_forecast_day2.add_actor(
+        new St.Icon({
+          icon_size: 32,
+          gicon: Gio.icon_new_for_string(
+            Me.dir.get_path() + '/svgs/weather/clear-day.svg',
+          ),
+          y_expand: true,
+          y_align: Clutter.ActorAlign.CENTER,
+          x_expand: true,
+          x_align: Clutter.ActorAlign.CENTER,
+          style: 'padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
+      weather_forecast_day2.add_actor(
+        new St.Label({
+          text: '26°C',
+          style: 'padding-top: 10px; padding-bottom: 10px;',
+        }),
+      );
+      forecast_boxes.add_actor(weather_forecast_day2);
+      // weather_forecast -> day 2
 
       // weather_forecast -> day 3
       let weather_forecast_day3 = new St.BoxLayout({
@@ -282,9 +273,6 @@ const MyPopup = GObject.registerClass(
       this.menu.box.add_child(forecast_widget);
       // forecast_widget
 
-
-
-
       // footer_widget
       let bin = new St.Widget({
         style: '',
@@ -292,7 +280,7 @@ const MyPopup = GObject.registerClass(
       });
       bin.add_actor(
         new St.Label({
-          style:'font-size: 10px; ',
+          style: 'font-size: 10px; ',
           text: ' 資料內容由香港天文台提供 ',
           x_expand: true,
         }),
